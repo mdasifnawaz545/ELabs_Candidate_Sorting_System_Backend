c481a6ba7caa4249ad34447529ddc48b30aef2b9
Md Asif Nawaz <mdasifnawaz545@gmail.com>
2024-07-17 09:57:31 +0530

    More Files are added

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,10 @@
 "use strict";
 let quote = `Salaam`;
 console.log(quote);
+const obj = {
+    name: 'MOHAMMAD ASIF NAWAZ',
+    batch: 2026
+};
+for (let transaction in obj) {
+    console.log(obj[transaction]);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
 let quote = `Salaam`
 console.log(quote);
+
+interface transactionObject{
+    [key:string]:string|number|boolean
+}
+
+interface newTransactionObject extends transactionObject{
+    name:string,
+    batch:number
+}
+
+const obj:newTransactionObject={
+    name:'MOHAMMAD ASIF NAWAZ',
+    batch:2026
+}
+
+for(let transaction in obj){
+    console.log(obj[transaction]);
+}
