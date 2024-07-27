8ff5950c4265fc195960fdc6ce84182ec57049af
Md Asif Nawaz <mdasifnawaz545@gmail.com>
2024-07-27 22:33:15 +0530

    Upload today's Files

--- ReactWithTypescript/src/App.tsx
+++ ReactWithTypescript/src/App.tsx
@@ -3,17 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ListItem from './Components/ListItem'
+import Reducer from './Components/Reducer'
+import { contextProvider, initialState } from './context/CounterContext'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <>
-   <ListItem list={['Dates','apple','Mango']} add={(item:string)=>(
-    <span>{item}</span>
-   )}/>
-   
-   </>
+    <>
+      <contextProvider count={initialState.count} text={initialState.text}>
+        {/* <ListItem list={['Dates', 'apple', 'Mango']} add={(item: string) => (
+        <span>{item}</span>
+      )} /> */}
+
+      </contextProvider>
+    </>
+
   )
 }
 
